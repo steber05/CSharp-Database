99f4ce19966c914b49775ff1fd1826bdc9cec11a
Stevie.wintec <steber05@student.wintec.ac.nz>
2019-03-04 09:25:37 +1300

    commit

---
+++ databaseApp/databaseApp/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace databaseApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disconnectToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.queryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.results = new System.Windows.Forms.DataGridView();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.results2 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.results)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.results2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.databaseToolStripMenuItem,
+            this.queryToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(537, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // databaseToolStripMenuItem
+            // 
+            this.databaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectToolStripMenuItem,
+            this.disconnectToolStripMenuItem});
+            this.databaseToolStripMenuItem.Name = "databaseToolStripMenuItem";
+            this.databaseToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.databaseToolStripMenuItem.Text = "Database";
+            // 
+            // connectToolStripMenuItem
+            // 
+            this.connectToolStripMenuItem.Name = "connectToolStripMenuItem";
+            this.connectToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.connectToolStripMenuItem.Text = "Connect";
+            this.connectToolStripMenuItem.Click += new System.EventHandler(this.connectToolStripMenuItem_Click);
+            // 
+            // disconnectToolStripMenuItem
+            // 
+            this.disconnectToolStripMenuItem.Enabled = false;
+            this.disconnectToolStripMenuItem.Name = "disconnectToolStripMenuItem";
+            this.disconnectToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.disconnectToolStripMenuItem.Text = "Disconnect";
+            this.disconnectToolStripMenuItem.Click += new System.EventHandler(this.disconnectToolStripMenuItem_Click);
+            // 
+            // queryToolStripMenuItem
+            // 
+            this.queryToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem});
+            this.queryToolStripMenuItem.Name = "queryToolStripMenuItem";
+            this.queryToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.queryToolStripMenuItem.Text = "Query";
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5;
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.runToolStripMenuItem.Text = "Run full query";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(227, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter a table name";
+            // 
+            // results
+            // 
+            this.results.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.results.Location = new System.Drawing.Point(0, 66);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(537, 150);
+            this.results.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 222);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(93, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter a ID";
+            // 
+            // results2
+            // 
+            this.results2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.results2.Location = new System.Drawing.Point(0, 261);
+            this.results2.Name = "results2";
+            this.results2.Size = new System.Drawing.Size(537, 150);
+            this.results2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(537, 411);
+            this.Controls.Add(this.results2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Database app";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.results)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.results2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem databaseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem connectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disconnectToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem queryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView results;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView results2;
+    }
+}
+
---
+++ databaseApp/databaseApp/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace databaseApp
+{
+    public partial class Form1 : Form
+    {
+        String connString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\code\\local\\database\\databaseapp\\FarmInfomation.accdb;Persist Security Info=False";
+        String errorMsg = "";
+        String q1 = "";
+        OleDbConnection conn = null;
+        bool validEntry = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void connectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new OleDbConnection(connString);
+                conn.Open();
+                disconnectToolStripMenuItem.Enabled=true;
+                connectToolStripMenuItem.Enabled=false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectToolStripMenuItem.PerformClick();
+        }
+
+        private void disconnectToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            conn.Close();
+            disconnectToolStripMenuItem.Enabled = false;
+            connectToolStripMenuItem.Enabled = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            disconnectToolStripMenuItem.PerformClick();
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            runTableQuery();
+            if(validEntry)
+            {
+                runIDQuery();
+            }
+            this.Cursor = Cursors.Default;
+        }
+
+        private void runTableQuery()
+        {
+            string check = textBox1.Text.ToLower();
+            errorMsg = "";
+
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Enter a table name");
+            }
+            else
+            {
+                switch (check)
+                {
+                    case "commodity":
+                        q1 = "SELECT * FROM [COMMODITY PRICES]";
+                        validEntry = true;
+                        break;
+                    case "cows":
+                        q1 = "SELECT * FROM COWS";
+                        validEntry = true;
+                        break;
+                    case "dogs":
+                        q1 = "SELECT * FROM DOGS";
+                        validEntry = true;
+                        break;
+                    case "goats":
+                        q1 = "SELECT * FROM GOATS";
+                        validEntry = true;
+                        break;
+                    case "sheep":
+                        q1 = "SELECT * FROM SHEEP";
+                        validEntry = true;
+                        break;
+                    default:
+                        MessageBox.Show("\tEnter a valid table name\n(Commodity, Cows, Dogs, Goats, Sheep)");
+                        break;
+                }
+            }
+            if (validEntry)
+            {
+                try
+                {
+                    OleDbCommand cmd = new OleDbCommand(q1, conn);
+                    OleDbDataAdapter a = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    a.SelectCommand = cmd;
+                    a.Fill(dt);
+
+                    results.DataSource = dt;
+                    results.AutoResizeColumns();
+                }
+                catch (Exception ex)
+                {
+                    errorMsg = ex.Message;
+                    MessageBox.Show(errorMsg);
+                }
+
+            }
+        }
+
+        private void runIDQuery()
+        {
+            //adjust query to suit the table entered in table textbox
+            string table = textBox1.Text;
+            string q1 = "SELECT * FROM " + table + " WHERE ID = " + textBox2.Text;
+            
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("Please enter a table name first");
+            }            
+            //Had no proper way to check values between as they adjusted per table
+            //so checked if ID textbox is empty
+            else if(textBox2.Text == "")
+            {
+                MessageBox.Show("Enter a ID");
+            }
+            else if(validEntry)
+            {
+                try
+                {
+                    OleDbCommand cmd = new OleDbCommand(q1, conn);
+                    OleDbDataAdapter a = new OleDbDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    a.SelectCommand = cmd;
+                    a.Fill(dt);
+
+                    results2.DataSource = dt;
+                    results2.AutoResizeColumns();
+                    //Second count checks if a table is returned 
+                    //if no rows return then it must be a invalid ID
+                    //display a message to enter a valid ID
+                    if (dt.Rows.Count < 1)
+                    {
+                        MessageBox.Show("Enter a valid ID");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    errorMsg = ex.Message;
+                    MessageBox.Show(errorMsg);
+                }
+            }   
+        }
+
+        private void tableButton_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            runTableQuery();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            runIDQuery();
+            this.Cursor = Cursors.Default;
+        }
+    }
+}
